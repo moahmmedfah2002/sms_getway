c88810ca65abe31fd18e372a0605b36f1539155a
Khabir <hakim1khabir2@gmail.com>
2025-05-10 19:15:36 +0100

    remove keycloak

--- mysql-init/init.sql
+++
@@ -1,6 +0,0 @@
--- Create the Keycloak database
-CREATE DATABASE IF NOT EXISTS keycloak CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Grant privileges to the keycloak user
-GRANT ALL PRIVILEGES ON keycloak.* TO 'keycloak'@'%';
-FLUSH PRIVILEGES;
